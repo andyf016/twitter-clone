6961d88d8f6bf2d0b5fc282654f02442ad308dfd
Andrew Fillenwarth <andyf016@gmail.com>
2020-09-01 23:21:18 -0400

    methods added to CustomUser

--- twitteruser/models.py
+++ twitteruser/models.py
@@ -6,7 +6,14 @@ class CustomUser(AbstractUser):
     bio = models.TextField(max_length=240, null=True, blank=True)
     age = models.IntegerField(null=True, blank=True)
     location = models.TextField(max_length=180, null=True, blank=True)
-    foll
+    # Ask about logic in model
+    def get_following(self):
+        following = FollowingUser.objects.filter(user_id=self.user_id)
+        return following
+    
+    def get_followers(self):
+        followers = FollowingUser.objects.filter(following_user_id=self.following_user_id)
+        return  followers
 
     def __str__(self):
         return self.username
@@ -15,7 +22,7 @@ class FollowingUser(models.Model):
     # modeled after code from unicdev on stackoverflow.com
     user_id = models.ForeignKey("CustomUser", related_name="following")
     following_user_id = models.ForeignKey("CustomUser", related_name="followers")
-    created = models.DateField(default=timezone.now)
+    created = models.DateField(default=timezone.now, editable=False)
 
     class Meta:
         # Prohibit following the same user more than once
