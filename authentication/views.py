06ea7e42c5ffd35dc618c92af6d6be6c064e8b1e
Andrew Fillenwarth <andyf016@gmail.com>
2020-09-02 21:20:23 -0400

    finish signup and login forms, working on views

--- authentication/views.py
+++ authentication/views.py
@@ -1,3 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from twitteruser.models import CustomUser, FollowingUser
+from twitteruser.forms import SignUpForm
--- twitteruser/views.py
+++ twitteruser/views.py
@@ -1,4 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponseRedirect, reverse
+from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.decorators import login_required
+
+from twitteruser.models import CustomUser, FollowingUser
+from twitteruser.forms import SignUpForm
 
 # Create your views here.
 """
